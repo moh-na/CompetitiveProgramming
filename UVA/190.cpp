08d97f574959e6cb6ea05e822053376c85e0b1d9
MohamedNabil97 <moh.ahmednabil@gmail.com>
2017-07-03 22:44:59 +0200

    Update 190.cpp

--- UVA/190.cpp
+++ UVA/190.cpp
@@ -46,6 +46,67 @@ double fixAngle(double A){return A > 1 ? 1 : (A < -1 ? -1 : A);}
 double dcmp(double a,double b){return fabs(a-b)<EPS?0 : a>b? 1:-1; }
 
 
+int intersection_straightlines(point a,point b, point c,point d,point& intersection1){ //0 means no intersection //1means intersection //-1 more than one
+
+     point u=vec(a,b);
+     point v=vec(c,d);
+     point w=vec(c,a);
+
+     double s1=crossp(v,w);
+     double s2=crossp(u,w);
+    double D=crossp(u,v);
+
+    if(D==0) {// They are collinear ,parallel
+        if(s1!=0 || s2!=0) return 0; // not colinear
+        //Colinear Cases (check for overlap)
+        return -1;
+    }
+
+    double t11=s1/D;
+    double t22=s2/D;
+
+
+    intersection1 = a + t11 * u;
+      return 1;
+
+
+
+}
+
+
+pair<double,point> circle_3points(point a,point b,point c){
+
+    point w=vec(a,b);
+    point v=vec(b,c);
+
+    point perpw=point(w.Y,-1*w.X);
+    point perpv=point(v.Y,-1*v.X);
+
+    point midw=0.5*(a+b);
+    point midv=0.5*(b+c);
+
+    point endw=midw+perpw;
+    point endv=midv+perpv;
+
+    point inter;
+
+    intersection_straightlines(midw,endw,midv,endv,inter);
+
+    return {length(vec(inter,a)),inter};
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
 int main(){
 
   int t;
