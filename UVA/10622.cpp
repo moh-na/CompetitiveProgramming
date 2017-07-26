0b6474fce476d08761070b7dc1c6c9e3b75d5f72
MohamedNabil97 <moh.ahmednabil@gmail.com>
2017-07-26 21:50:00 +0200

    Create 10622.cpp

---
+++ UVA/10622.cpp
@@ -0,0 +1,81 @@
+/*Mohamed Ahmed Nabil
+Just brute force
+*/
+#include <bits/stdc++.h>
+#include <complex>
+using namespace std;
+
+#define lp(i,n) for(int i=0; i<n; i++)
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<long long ,long long>
+#define ff first
+#define ss second
+#define nl "\n"
+
+#define EPS 1e-6
+#define OO 1000000000
+
+#define on(i,n) i=i|(1<<n)
+#define off(i,n) i=i& (~(1<<n))
+
+typedef  complex<double> point;
+#define X real()
+#define Y imag()
+
+#define vec(a,b) (b-a)
+#define angle(a) (atan2(((a).Y),((a).X)))
+
+#define length(a) (hypot(((a).real()),((a).imag())))
+#define normalize(a) ((a)/(length(a)))
+
+#define dotp(a,b) ((conj(a)*(b)).real())
+#define crossp(a,b) ((conj(a)*b).imag())
+#define same(a,b) ((dcmp(((a).X),((b).X))==0 )&& (dcmp(((a).Y),((b).Y))==0))
+
+#define lengthSqr(a) (dp((a),(a)))
+
+#define rotateO(p,ang) ((p)*exp(point(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define reflectO(v,m)  (conj((v)/(m))*(m))
+
+#define debugme freopen("out.txt","w",stdout)
+#define endp return 0;
+const double PI= acos(-1.0);
+double fixAngle(double A){return A > 1 ? 1 : (A < -1 ? -1 : A);}
+double dcmp(double a,double b){return fabs(a-b)<EPS?0 : a>b? 1:-1; }
+
+
+int main(){
+    //debugme;
+  ll  n=2;
+
+
+
+
+
+   while( cin>>n){
+    if(!n) return 0;
+    int x=n;
+    int ans=1;
+    for(int i=2; i<33; i++){
+        if(n<0)
+        for(int j=2; dcmp(pow(j,i),-1*n)!=1 ; j++){
+           if(i%2==0) break;
+            if(dcmp(powl(j,i),-1*n)==0)
+                ans=i;
+
+        }
+        else
+        for(int j=2; dcmp(pow(j,i),n)!=1; j++){
+
+            if(dcmp(powl(j,i),n)==0) ans=i;
+
+        }
+
+
+    }
+    cout<<ans<<endl;
+   }
+}
