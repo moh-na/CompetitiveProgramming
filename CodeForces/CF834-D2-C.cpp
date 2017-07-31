da977f0e4f8a41218b429cfe5595b2e7e79af917
MohamedNabil97 <moh.ahmednabil@gmail.com>
2017-07-31 19:31:44 +0200

    Create CF834-D2-C.cpp

---
+++ CodeForces/CF834-D2-C.cpp
@@ -0,0 +1,75 @@
+/*
+Mohamed Ahmed Nabil
+Since multiplying by k to first and k^2 to second gives us k^3 when we multiply a and b
+
+the answer can be done by checking if a*b is a cubic number and also that the cubic root is divisible by a and b
+
+*/
+#include <bits/stdc++.h>
+#include <complex>
+using namespace std;
+
+#define lp(i,n) for(ll i=0; i<n; i++)
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<ll ,ll>
+#define ff first
+#define ss second
+#define nl "\n"
+
+#define EPS 1e-6
+#define OO 1000000000
+
+#define on(i,n) i=i|(1<<n)
+#define off(i,n) i=i& (~(1<<n))
+
+typedef  complex<double> point;
+#define X real()
+#define Y imag()
+
+#define vec(a,b) (b-a)
+#define angle(a) (atan2(((a).Y),((a).X)))
+
+#define length(a) (hypot(((a).real()),((a).imag())))
+#define normalize(a) ((a)/(length(a)))
+
+#define dotp(a,b) ((conj(a)*(b)).real())
+#define crossp(a,b) ((conj(a)*b).imag())
+#define same(a,b) ((dcmp(((a).X),((b).X))==0 )&& (dcmp(((a).Y),((b).Y))==0))
+
+#define lengthSqr(a) (dp((a),(a)))
+
+#define rotateO(p,ang) ((p)*exp(poll(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define reflectO(v,m)  (conj((v)/(m))*(m))
+
+#define debugme freopen("out.txt","w",stdout)
+#define endp return 0;
+const double PI= acos(-1.0);
+double fixAngle(double A){return A > 1 ? 1 : (A < -1 ? -1 : A);}
+double dcmp(double a,double b){return fabs(a-b)<EPS?0 : a>b? 1:-1; }
+const int MAX=(int)1e3+5;
+
+
+int main(){
+
+
+    ll a,b;
+    int n;
+    scanf("%d",&n);
+    lp(i,n){
+        scanf("%I64d%I64d",&a,&b);
+        ll s=a*b;
+
+
+        ll cb=(cbrt(s)+0.1);
+
+        if( cb*cb*cb==s  && a%cb==0 && b%cb==0){
+            puts("Yes");
+        }else{
+            puts("No");
+        }
+    }
+
+}
