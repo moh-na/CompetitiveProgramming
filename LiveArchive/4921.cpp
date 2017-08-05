299edc043e1ed1f551d0d733046e00e8e6002f29
MohamedNabil97 <moh.ahmednabil@gmail.com>
2017-08-06 01:25:45 +0200

    Create 4921.cpp

---
+++ LiveArchive/4921.cpp
@@ -0,0 +1,75 @@
+/* Direct implementation of the problem */
+#include <bits/stdc++.h>
+#include <complex>
+using namespace std;
+
+#define lp(i,n) for(ll i=0; i<n; i++)
+#define ll long long
+#define pb push_back
+#define mp make_pair
+
+#define pii pair<int ,int>
+#define ff first
+#define ss second
+#define nl "\n"
+
+#define EPS 1e-6
+#define OO 100000000
+
+#define on(i,n) i=i|(1<<n)
+#define off(i,n) i=i& (~(1<<n))
+
+typedef  complex<int> point;
+#define X real()
+#define Y imag()
+
+#define vec(a,b) (b-a)
+#define angle(a) (atan2(((a).Y),((a).X)))
+
+#define length(a) (hypot(((a).real()),((a).imag())))
+#define normalize(a) ((a)/(length(a)))
+
+#define dotp(a,b) ((conj(a)*(b)).real())
+#define crossp(a,b) ((conj(a)*b).imag())
+#define same(a,b) ((dcmp(((a).X),((b).X))==0 )&& (dcmp(((a).Y),((b).Y))==0))
+
+#define lengthSqr(a) (dp((a),(a)))
+
+#define rotateO(p,ang) ((p)*exp(poll(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define reflectO(v,m)  (conj((v)/(m))*(m))
+
+#define debugme freopen("out.txt","w",stdout)
+#define endp return 0;
+const double PI= acos(-1.0);
+double fixAngle(double A){return A > 1 ? 1 : (A < -1 ? -1 : A);}
+double dcmp(double a,double b){return fabs(a-b)<EPS?0 : a>b? 1:-1; }
+const int MAXN=500;
+
+int main(){
+ while(1){
+   string s;
+   cin>>s;
+   if(s=="#") break;
+
+   int a=0,y=0,n=0;
+
+   for(char x:s){
+        if(x=='A') a++;
+        if(x=='Y') y++;
+        if(x=='N') n++;
+
+   }
+   if((double)a>= (double)(s.size()/2.0) ){
+     cout<<"need quorum";
+
+   }else if(y>n){
+    cout<<"yes";
+   }else if(n>y){
+    cout<<"no";
+   }else{
+    cout<<"tie";
+   }
+   cout<<endl;
+ }
+}
