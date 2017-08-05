a37a849ca9bba8ff39c875a67ff61484094079d9
MohamedNabil97 <moh.ahmednabil@gmail.com>
2017-08-06 01:28:58 +0200

    Create 1023.cpp

---
+++ Timus/1023.cpp
@@ -0,0 +1,68 @@
+/* We need to reach a number for which mod k =0
+
+so we pick k-1
+So If The oponent adds any number from (1->k-1) We add(k-opponents number)
+
+brute force from 3 to find lowest k
+*/ 
+#include <bits/stdc++.h>
+#include <complex>
+using namespace std;
+
+#define lp(i,n) for(ll i=0; i<n; i++)
+#define ll long long
+#define pb push_back
+#define mp make_pair
+
+#define pii pair<int ,int>
+#define ff first
+#define ss second
+#define nl "\n"
+
+#define EPS 1e-6
+#define OO 100000000
+
+#define on(i,n) i=i|(1<<n)
+#define off(i,n) i=i& (~(1<<n))
+
+typedef  complex<int> point;
+#define X real()
+#define Y imag()
+
+#define vec(a,b) (b-a)
+#define angle(a) (atan2(((a).Y),((a).X)))
+
+#define length(a) (hypot(((a).real()),((a).imag())))
+#define normalize(a) ((a)/(length(a)))
+
+#define dotp(a,b) ((conj(a)*(b)).real())
+#define crossp(a,b) ((conj(a)*b).imag())
+#define same(a,b) ((dcmp(((a).X),((b).X))==0 )&& (dcmp(((a).Y),((b).Y))==0))
+
+#define lengthSqr(a) (dp((a),(a)))
+
+#define rotateO(p,ang) ((p)*exp(poll(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define reflectO(v,m)  (conj((v)/(m))*(m))
+
+#define debugme freopen("out.txt","w",stdout)
+#define endp return 0;
+const double PI= acos(-1.0);
+double fixAngle(double A){return A > 1 ? 1 : (A < -1 ? -1 : A);}
+double dcmp(double a,double b){return fabs(a-b)<EPS?0 : a>b? 1:-1; }
+const int MAXN=700;
+
+
+int main(){
+    int k;
+    cin>>k;
+
+
+    for(int i=3; i<k+1; i++){
+        if(k%i==0){
+            cout<<i-1<<endl;
+            return 0;
+        }
+    }
+
+}
