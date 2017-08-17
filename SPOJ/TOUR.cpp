e6704589dccc32156415dff7b21ed8c0c44d80f5
MohamedNabil97 <moh.ahmednabil@gmail.com>
2017-08-17 04:01:07 +0200

    Create TOUR.cpp

---
+++ SPOJ/TOUR.cpp
@@ -0,0 +1,104 @@
+/* loop over all nodes Check if a node can reach everyother node and if so ++ ans*/
+#include <bits/stdc++.h>
+#include <complex>
+using namespace std;
+
+#define lp(i,n) for(ll i=0; i<n; i++)
+#define ll long long
+#define pb push_back
+#define mp make_pair
+
+#define pii pair<int,int>
+#define ff first
+#define ss second
+#define nl "\n"
+
+#define EPS 1e-9
+#define OO 100000000
+
+#define on(i,n) i=i|(1<<n)
+#define off(i,n) i=i& (~(1<<n))
+
+typedef  complex<int> point;
+#define X real()
+#define Y imag()
+
+#define vec(a,b) (b-a)
+#define angle(a) (atan2(((a).Y),((a).X)))
+
+#define length(a) (hypot(((a).real()),((a).imag())))
+#define normalize(a) ((a)/(length(a)))
+
+#define dotp(a,b) ((conj(a)*(b)).real())
+#define crossp(a,b) ((conj(a)*b).imag())
+#define same(a,b) ((dcmp(((a).X),((b).X))==0 )&& (dcmp(((a).Y),((b).Y))==0))
+
+#define lengthSqr(a) (dp((a),(a)))
+
+#define rotateO(p,ang) ((p)*exp(poll(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define reflectO(v,m)  (conj((v)/(m))*(m))
+
+#define debugme freopen("out.txt","w",stdout)
+#define endp return 0;
+const double PI= acos(-1.0);
+double fixAngle(double A){return A > 1 ? 1 : (A < -1 ? -1 : A);}
+double dcmp(double a,double b){return fabs(a-b)<EPS?0 : a>b? 1:-1; }
+
+
+/* int dx[] = {1,-1,0,0} , dy[] = {0,0,1,-1}; */ // 4 Direction
+/* int dx[] = {1,-1,0,0,1,1,-1,-1} , dy[] = {0,0,1,-1,1,-1,1,-1}; */ // 8 Direction
+/* int dx[] = {1,-1,1,-1,2,2,-2,-2} , dy[] = {2,2,-2,-2,1,-1,1,-1}; */ // Knight Direction
+/* int dx[] = {2,-2,1,1,-1,-1} , dy[] = {0,0,1,-1,1,-1}; */ // Hexagonal Direction
+
+
+
+vector<vector<int>> graph;
+
+int visited[2000];
+
+int check(int idx){
+    visited[idx]=1;
+
+    int cnt=0;
+    for(int child: graph[idx]){
+        if(visited[child]) continue;
+        cnt+= check (child);
+    }
+
+    return 1+ cnt;
+
+
+}
+
+int main(){
+    int t;
+    cin >>t;
+    while(t--){
+    graph.clear();
+    int n;
+    cin>>n;
+    graph.resize(n);
+    int m;
+    lp(i,n){
+        cin>>m;
+
+        lp(j,m){
+            int x;
+            cin>>x; x--;
+            graph[x].pb(i);
+
+        }
+    }
+    int ans=0;
+    lp(i,n){
+        memset(visited,0,sizeof visited);
+
+        if(check(i)==n) ans++;
+
+    }
+    cout<<ans<<endl;
+
+    }
+}
+
