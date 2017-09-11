1593d5ceed0799787a2a0250411957a080850333
MohamedNabil97 <moh.ahmednabil@gmail.com>
2017-09-11 23:54:08 +0200

    Update SUBBXOR.cpp

--- CodeChef/SUBBXOR.cpp
+++ CodeChef/SUBBXOR.cpp
@@ -2,6 +2,33 @@
 
 Direct usage of a binary trie to find The number of elements
 
+First we maintain a binary trie which holds the binary values of the numbers each node of the trie is stored in it the address of its children and the number of times we have recorded numbers through this node.
+The trie maintins the number starting From the bit with the most weight to the one with the least (Leafs will be the bits with the least weight)
+
+Find count finds the number of elements in the trie which XOR "number" will give us a result less than K
+First Lets look at how we can find in a binary trie the number of elements less than K
+Let bit_trie be the current bit of the trie
+Let bit_trie be the current bit of K
+
+If bit_trie == 0 and bit k ==0 then continue traversing
+If bit_trie ==0 and bit k==1 return 0
+If bit_trie==1 and bitk ==0 return the cnt variable
+
+Now How do we find the number of elements in the binary trie which XOR a number will give us a number less than K
+
+Look at the truth table of XOR
+A B Result
+0 0 0
+0 1 1
+1 0 1
+1 1 0
+
+Whenever B=1 The result is the flipping of A
+So If The value of the bit we are working on is 1 it is the equivalent of flipping the Current bit of the trie 0 is 1 and 1 is 0 otherwise it stays the same
+
+In that way we have applied the XOR on our trie as we go now we need to see how can we find the numbers in the trie less than K after applying the XOR .. We do that As Explained above
+
+
 */
 #include <bits/stdc++.h>
 
