59d10ebcb52e70e4d436553dc0f9eb4e1da3f3bf
MohamedNabil97 <moh.ahmednabil@gmail.com>
2018-01-29 22:44:39 +0200

    Create CF280-D1-C.cpp

---
+++ CodeForces/CF280-D1-C.cpp
@@ -0,0 +1,97 @@
+/*
+As in Editorial
+
+Summary:
+"chance of it additing 1 to total expectation
+is the chance of it being selected before its parents"
+*/
+
+#include<bits/stdc++.h>
+#include <iomanip>
+#include <vector>
+#include<cstdio>
+#include <cstdlib>
+#include <complex>
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <cstring>
+using namespace std;
+#define ll long long
+
+
+#define pii pair<long long, long long>
+#define endl "\n"
+
+#define EPS 1e-7
+#define OO 100000000
+
+#define on(i,n) i=i|(1<<n)
+#define off(i,n) i=i& (~(1<<n))
+
+typedef  complex<double> point;
+#define X real()
+#define Y imag()
+
+#define vec(a,b) (b-a)
+#define angle(a) (atan2(((a).Y),((a).X)))
+
+#define length(a) (hypot(((a).real()),((a).imag())))
+#define normalize(a) ((a)/(length(a)))
+
+#define dotp(a,b) ((conj(a)*(b)).real())
+#define crossp(a,b) ((conj(a)*(b)).imag())
+#define same(a,b) ((dcmp(((a).X),((b).X))==0 )&& (dcmp(((a).Y),((b).Y))==0))
+
+#define lengthSqr(a) (dp((a),(a)))
+
+#define rotateO(p,ang) ((p)*exp(poll(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define reflectO(v,m)  (conj((v)/(m))*(m))
+
+#define debugme freopen("out.txt","w",stdout)
+#define endp return 0;
+
+#define goleft(L,R)  L, (L+R)/2 ,idx*2
+#define goright(L,R) (L+R)/2+1, R, idx*2+1
+const double PI= acos(-1.0);
+long double fixAngle(long double A){return A > 1 ? 1 : (A < -1 ? -1 : A);}
+double dcmp( double a,double b){return fabs(a-b)<EPS?0 : a>b? 1:-1; }
+
+int n;
+vector<vector<int>> graph;
+double solve(){
+
+    int depth[n+1];
+    memset(depth,0,sizeof depth);
+
+    queue<int> q;
+    q.push(1);
+    depth[1]=1;
+    double ret=0;
+    while(!q.empty()){
+        int f=q.front(); q.pop();
+        ret+=1.0/depth[f];
+        for(auto child:graph[f]){
+            if(depth[child]!=0) continue;
+            depth[child]=depth[f]+1;
+            q.push(child);
+        }
+    }
+    return ret;
+
+}
+
+int main(){
+    cin>>n;
+    graph.resize(n+1);
+    for(int i=1; i<n; i++){
+        int a,b;
+        cin>>a>>b;
+
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+
+    }
+    cout<<setprecision(10)<<fixed<<solve()<<endl;
+}
