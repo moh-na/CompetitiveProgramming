e04985b822244de2fb9cb932272539dbc835bee9
MohamedNabil97 <moh.ahmednabil@gmail.com>
2018-02-03 19:14:08 +0200

    Create CF915-D2-B.cpp

---
+++ CodeForces/CF915-D2-B.cpp
@@ -0,0 +1,117 @@
+/*
+Iterate and minmize
+*/
+#include<bits/stdc++.h>
+#include <iomanip>
+#include <vector>
+#include<cstdio>
+#include <cstdlib>
+#include <complex>
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <cstring>
+using namespace std;
+#define ll long long
+
+
+#define pii pair<long long, long long>
+#define endl "\n"
+
+#define EPS 1e-7
+#define OO 100000000
+
+#define on(i,n) i=i|(1<<n)
+#define off(i,n) i=i& (~(1<<n))
+
+typedef  complex<double> point;
+#define X real()
+#define Y imag()
+
+#define vec(a,b) (b-a)
+#define angle(a) (atan2(((a).Y),((a).X)))
+
+#define length(a) (hypot(((a).real()),((a).imag())))
+#define normalize(a) ((a)/(length(a)))
+
+#define dotp(a,b) ((conj(a)*(b)).real())
+#define crossp(a,b) ((conj(a)*(b)).imag())
+#define same(a,b) ((dcmp(((a).X),((b).X))==0 )&& (dcmp(((a).Y),((b).Y))==0))
+
+#define lengthSqr(a) (dp((a),(a)))
+
+#define rotateO(p,ang) ((p)*exp(poll(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define reflectO(v,m)  (conj((v)/(m))*(m))
+
+#define debugme freopen("out.txt","w",stdout)
+#define endp return 0;
+
+#define goleft(L,R)  L, (L+R)/2 ,idx*2
+#define goright(L,R) (L+R)/2+1, R, idx*2+1
+const double PI= acos(-1.0);
+long double fixAngle(long double A){return A > 1 ? 1 : (A < -1 ? -1 : A);}
+double dcmp( double a,double b){return fabs(a-b)<EPS?0 : a>b? 1:-1; }
+
+
+int main(){
+
+   int n,pos,l,r;
+    cin>>n>>pos>>l>>r;
+    int ans=0;
+    int ans2=0;
+
+    int pos2=pos;
+    if(pos>=l && pos<=r){
+        if(l!=1){
+            ans=pos-l;
+            ans++;
+            pos=l;
+        }
+        if(r!=n){
+            ans+=r-pos;
+            ans++;
+
+        }
+        
+        if(r!=n){
+        ans2=r-pos2;
+        ans2+=(r!=n);
+        pos2=r;
+        }
+        if(l!=1){
+            ans2+=pos2-l;
+            ans2++;
+
+        }
+        cout<<min(ans,ans2);
+        return 0;
+
+
+    }if(pos<l){
+        ans+=l-pos;
+        if(l!=1) ans++;
+        if(r!=n){
+            ans+=r-l;
+            ans++;
+
+        }
+
+        cout<<ans<<endl;
+        return 0;
+
+    }
+    if(pos>r){
+         ans2=pos-r;
+        ans2+=(r!=n);
+
+        if(l!=1){
+            ans2+=r-l;
+            ans2++;
+
+        }
+           cout<<ans2<<endl;
+        return 0;
+
+    }
+}
