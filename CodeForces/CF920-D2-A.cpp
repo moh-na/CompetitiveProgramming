32f99e3fb6aa5bbda1c8c97f48a95d001cb98afa
MohamedNabil97 <moh.ahmednabil@gmail.com>
2018-02-03 19:47:12 +0200

    Create CF920-D2-A.cpp

---
+++ CodeForces/CF920-D2-A.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+#include <iomanip>
+#include <vector>
+#include<cstdio>
+#include <cstdlib>
+#include <complex>
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <cstring>
+using namespace std;
+#define ll long long
+
+
+#define pii pair<long long, long long>
+#define endl "\n"
+
+#define EPS 1e-7
+#define OO 100000000
+
+#define on(i,n) i=i|(1<<n)
+#define off(i,n) i=i& (~(1<<n))
+
+typedef  complex<double> point;
+#define X real()
+#define Y imag()
+
+#define vec(a,b) (b-a)
+#define angle(a) (atan2(((a).Y),((a).X)))
+
+#define length(a) (hypot(((a).real()),((a).imag())))
+#define normalize(a) ((a)/(length(a)))
+
+#define dotp(a,b) ((conj(a)*(b)).real())
+#define crossp(a,b) ((conj(a)*(b)).imag())
+#define same(a,b) ((dcmp(((a).X),((b).X))==0 )&& (dcmp(((a).Y),((b).Y))==0))
+
+#define lengthSqr(a) (dp((a),(a)))
+
+#define rotateO(p,ang) ((p)*exp(poll(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define reflectO(v,m)  (conj((v)/(m))*(m))
+
+#define debugme freopen("out.txt","w",stdout)
+#define endp return 0;
+
+#define goleft(L,R)  L, (L+R)/2 ,idx*2
+#define goright(L,R) (L+R)/2+1, R, idx*2+1
+const double PI= acos(-1.0);
+long double fixAngle(long double A){return A > 1 ? 1 : (A < -1 ? -1 : A);}
+double dcmp( double a,double b){return fabs(a-b)<EPS?0 : a>b? 1:-1; }
+const int MAXN=600;
+vector<pii> graph[MAXN];
+int visited[MAXN];
+int dfs_num[MAXN];
+int dfs_time=1;
+int blocked=-1;
+int bfs(int idx){
+
+}
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+    int n,k;
+    cin>>n>>k;
+    vector<ll>data(k);
+
+    for(int i=0; i<k; i++){
+        cin>>data[i];
+    }
+    ll mx=-1;
+    for(int i=1; i<=n; i++){
+        ll mn=OO;
+        for(auto place:data){
+            mn=min(abs(place-i)+1,mn);
+        }
+        mx=max(mn,mx);
+
+    }
+    cout<<mx<<endl;
+    }
+}
